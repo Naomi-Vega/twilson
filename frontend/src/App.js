ebc6d669cad1fc6b23afa427d571b52ce721b9c8
Mauricio Tomaz <mtomaz@whiteoutsolutions.com>
2022-07-28 16:31:01 -0700

    Add fragment to solve single parent element issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <header>
-      <h2>
-
-      </h2>
-      <p>
-
-      </p>
-    </header>
+    <>
+      <header>
+        <h2>Twootr</h2>
+        <p>Write a new Twoot</p>
+      </header>
+      <section>
+        
+      </section>
+    </>
   );
 }
 
