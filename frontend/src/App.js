6768025fa9a2f3215b737337a3cebe9cda6939cf
Gabriel Soares de Brito <86431420+MendigoPower@users.noreply.github.com>
2022-07-28 17:02:42 -0700

    html update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,48 @@ function App() {
         <h2>Twootr</h2>
         <p>Write a new Twoot</p>
       </header>
+
       <section>
-        
+        <div>
+          <div>
+            <div>
+              <img></img>
+            </div>
+            <h2>John Doe</h2>
+            <h3>@john-doe</h3>
+          </div>
+        </div>
       </section>
+      
+      <section>
+        <h2> Compose Twoot </h2>
+        <textarea> What are humming about? </textarea>
+        <button> Twoot </button>
+        <span> Counter </span>
+      </section>
+
+      <section>
+        <article>
+          <div>
+            <img></img>
+            <p>Henry David Thoreau</p>
+            <p>@henry-david-thoreau</p>
+          </div>
+          <div>
+            <p>Many men go fishing....</p>
+          </div>
+          <div>
+            <p>twooted 22 hours ago</p>
+            <div>
+              <button>Flag button</button>
+              <button>Share Button</button>
+              <button>Like Button</button>
+            </div>
+          </div>
+        </article>
+      </section>
+
+
     </>
   );
 }
