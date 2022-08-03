ad4c162a7a742e56d5d882d227283a2a030180cd
Gabriel Soares de Brito <86431420+MendigoPower@users.noreply.github.com>
2022-08-03 15:19:33 -0700

    twoots data api

--- frontend/src/components/UserTwoots/UserTwoots.js
+++ frontend/src/components/UserTwoots/UserTwoots.js
@@ -1,34 +1,47 @@
 import StyledUserTwoots from './StyledUserTwoots';
 import axios from "axios";
-import React from "react";
-
-
+import React, { useEffect, useState } from "react";
 
 
 function UserTwoots() {
+    
+    const [twoots, setTwoots] = useState([]) 
+
+    useEffect(() => {
+        const url='http://localhost:8080/twoots';
+        axios.get(url).then((response) => {
+            setTwoots(response.data)
+            console.log(response)
+        })
+    },[])
+
     return (
         <StyledUserTwoots className="user-selector">
             <section>
-                <article>
-                    <div class="twoot-user">
-                        <img src="logo192.png" alt="User avatar"></img>
-                        <p>Henry David Thoreau</p>
-                        <div class="user-address">
-                            <p>@henry-david-thoreau</p>
+                {twoots.map((twilson) => {
+                        return(
+                    <article>
+                        <div class="twoot-user">
+                            <img src={`https://avatars.dicebear.com/api/bottts/${twilson.authorSlug}.svg`} alt="User avatar" />
+                            <p>{twilson.author}</p>
+                            <div class="user-address">
+                                <p>@{twilson.authorSlug}</p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="twoot">
-                        <p>Many men go fishing....</p>
-                    </div>
-                    <div class="twoot-timeframe">
-                        <p>twooted 22 hours ago</p>
-                        <div class="twoot-button">
-                            <button>Flag button</button>
-                            <button>Share Button</button>
-                            <button>Like Button</button>
+                        <div class="twoot">
+                            <p>{twilson.content}</p>
                         </div>
-                    </div>
-                </article>
+                        <div class="twoot-timeframe">
+                            <p>{twilson.dateAdded}</p>
+                            <div class="twoot-button">
+                                <button>Flag button</button>
+                                <button>Share Button</button>
+                                <button>Like Button</button>
+                            </div>
+                        </div>
+                    </article>
+                    )
+                })}
             </section>
         </StyledUserTwoots>
     );
