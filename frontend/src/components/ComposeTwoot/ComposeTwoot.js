a242a5b601adbe2da5dd35c0bdbd5f57a28592d0
Naomi-Vega <97486808+Naomi-Vega@users.noreply.github.com>
2022-08-05 16:36:29 -0400

    adding counter

--- frontend/src/components/ComposeTwoot/ComposeTwoot.js
+++ frontend/src/components/ComposeTwoot/ComposeTwoot.js
@@ -1,4 +1,95 @@
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
 import StyledComposeTwoot from './StyleComposeTwoot';
+
+
+
+const ComposeTwoot = (props) => {
+    const { isUpdate, setIsUpdate, user, isFocus, setIsFocus } = props;
+    const [wordCount, setWordCount] = useState(140);
+    const [isDisabled, setIsDisabled] = useState(false);
+    const textInputRef = useRef();
+
+
+    const handleCount = (e) => {
+        const length = e.target.value.length;
+        if (length > 140) {
+            alert("Exceeded the amount of characters");
+            setIsDisabled(true);
+            return;
+        }
+        isDisabled === true && setIsDisabled(false);
+        setWordCount(140 - length);
+    };
+
+    const handleMouseLeave = () => {
+        setIsFocus(false);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (textInputRef.current.value === "") {
+            alert("Invalid input");
+            return;
+        }
+
+        const newTwoot = {
+            author: user.firstName + " " + user.lastName,
+            content: textInputRef.current.value,
+            authorSlug:
+                user.firstName.toLowerCase() + "-" + user.lastName.toLowerCase(),
+        };
+
+        axios
+            .post("http://localhost:8080/twoot", { newTwoot })
+            .then((res) => {
+                setIsUpdate(!isUpdate);
+                textInputRef.current.value = "";
+                setWordCount(140);
+                setTimeout(() => { }, 500);
+            })
+            .catch((error) => console.log(error));
+    };
+
+    useEffect(() => {
+        isFocus && textInputRef.current.focus();
+    }, [isFocus]);
+
+    return (
+        <StyledComposeTwoot onSubmit={handleSubmit} id="composeTwoot">
+            <section className="compose-twoot-container">
+                <h2>Compose Twoot</h2>
+                <textarea
+                    id="twootText"
+                    onChange={handleCount}
+                    onBlur={handleMouseLeave}
+                    ref={textInputRef}
+                    type="text"
+                    placeholder="What are you humming about?"
+                />
+
+                <div class="composer-button-container">
+                    <div className="composer-button">
+                        <button disabled={isDisabled} className={isDisabled && "disable"}>
+                            Twoot
+                        </button>
+                    </div>
+                    <span>{wordCount}</span>
+                </div>
+            </section>
+        </StyledComposeTwoot >
+    );
+};
+
+
+
+
+
+
+
+
+
+/*
 
 function ComposeTwoot() {
     return (
@@ -12,7 +103,14 @@ function ComposeTwoot() {
                 <span class="textarea_counter">0/10</span>
             </div>
         </StyledComposeTwoot>
-    );    
+    );
 }
 
+*/
+
+
+
+
 export default ComposeTwoot;
+
+
--- frontend/src/components/ComposeTwoot/StyleComposeTwoot.js
+++ frontend/src/components/ComposeTwoot/StyleComposeTwoot.js
@@ -1,13 +1,28 @@
 import styled from 'styled-components';
 
 const StyledComposeTwoot = styled.section`
-    padding: 32px;
+    padding: 30px 70px;
+
+    h2 {
+      color: #3e4144;
+      margin: 0;
+    }
+
+    button {
+        background-color: #aa8caf;
+        color: white;
+        font-size: 15px;
+        border: none;
+        border-radius: 5px;
+        padding: 5px 20px;
+        text-decoration: solid; 
+      }
 
     textarea {
       width: 100%;
       border: none;
       padding-top: 15px;
-      border-bottom: 1px solid grey;
+      border-bottom: 1px solid black;
     }
 
     .composer-button-container {
@@ -17,7 +32,6 @@ const StyledComposeTwoot = styled.section`
       .composer-button, span {
         width: 50%;
       }
-
       span {
         text-align: right;
         color: gray;
